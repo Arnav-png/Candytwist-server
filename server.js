dad8812dd9ccfac62cff3a9188f6d230fe874e7d
__ARNAV__ <79275041+Arnav-png@users.noreply.github.com>
2022-06-04 01:07:19 +0530

    added port

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 
 const ScoreSchema = require("./Model/model")
+const port = process.env.PORT || 3000
 
 app.use(cors())
 app.use(express.json())
@@ -44,7 +45,7 @@ app.post("/score",async(req,res)=>{
     }
 })
 
-app.listen(3001,()=>{
-    console.log("app listening on port 3001")
+app.listen(port,()=>{
+    console.log(`app listening on port ${port}`)
 })
 
